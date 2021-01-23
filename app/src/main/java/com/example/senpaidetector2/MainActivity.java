c0dd805b32f06630e927f82a463b7e9564343857
Alex Ballmer <gnu3ra@riseup.net>
2021-01-23 16:31:27 -0600

    Aidl api

--- app/src/main/java/com/example/senpaidetector2/MainActivity.java
+++ app/src/main/java/com/example/senpaidetector2/MainActivity.java
@@ -1,18 +1,13 @@
 package com.example.senpaidetector2;
 
-import android.bluetooth.BluetoothGatt;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.Bundle;
-import android.os.FileObserver;
-import android.os.Handler;
 import android.os.IBinder;
-import android.os.Looper;
-import android.os.ParcelUuid;
+import android.os.RemoteException;
 import android.text.method.ScrollingMovementMethod;
-import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -22,67 +17,29 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.uscatterbrain.DeviceProfile;
 import com.example.uscatterbrain.ScatterRoutingService;
-import com.example.uscatterbrain.network.AdvertisePacket;
+import com.example.uscatterbrain.ScatterbrainAPI;
 import com.example.uscatterbrain.network.BlockHeaderPacket;
-import com.example.uscatterbrain.network.LuidPacket;
-import com.example.uscatterbrain.network.UpgradePacket;
-import com.example.uscatterbrain.network.bluetoothLE.BluetoothLEModule;
-import com.example.uscatterbrain.network.bluetoothLE.BluetoothLERadioModuleImpl;
 import com.example.uscatterbrain.network.bluetoothLE.CachedLEConnection;
-import com.example.uscatterbrain.network.bluetoothLE.CachedLEServerConnection;
-import com.example.uscatterbrain.network.wifidirect.WifiDirectBootstrapRequest;
-import com.example.uscatterbrain.network.wifidirect.WifiDirectRadioModule;
-import com.polidea.rxandroidble2.RxBleClient;
-import com.polidea.rxandroidble2.RxBleDevice;
-import com.polidea.rxandroidble2.RxBleServer;
-import com.polidea.rxandroidble2.ServerConfig;
-import com.polidea.rxandroidble2.Timeout;
-import com.polidea.rxandroidble2.scan.ScanFilter;
-import com.polidea.rxandroidble2.scan.ScanSettings;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.TimeUnit;
 
-import io.reactivex.Completable;
-import io.reactivex.CompletableObserver;
-import io.reactivex.Flowable;
 import io.reactivex.Observable;
-import io.reactivex.annotations.NonNull;
-import io.reactivex.disposables.Disposable;
-import io.reactivex.schedulers.Schedulers;
-import io.reactivex.subjects.BehaviorSubject;
-
-import static com.example.uscatterbrain.network.bluetoothLE.BluetoothLERadioModuleImpl.SERVICE_UUID;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = "MainActivity";
-    private ScatterRoutingService mService;
     private Switch mServiceToggle;
     private Switch mDiscoveryToggle;
     private TextView mStatusTextView;
     private TextView mLogsTextView;
     private Button mRefreshLogsButton;
     private Button mScanButton;
-    private Button mConnectGroupButton;
-    private Button mCreateGroupButton;
-    private Button mManualButton;
-    private Button mClientButton;
-    private Button mDeleteAllButton;
-    private RxBleServer mServer;
+    private ScatterbrainAPI binder;
     private boolean mBound;
-    private static final String BAD_PSK = "9XAqmPYDlNaDGcfv1inoOw==";
-    private Disposable p2pdisposable;
-    private Disposable manualDisposable = null;
     private final ConcurrentHashMap<String, Observable<CachedLEConnection>> connectionCache = new ConcurrentHashMap<>();
     private static final BlockHeaderPacket headerPacket = BlockHeaderPacket.newBuilder()
             .setApplication("fmef".getBytes())
@@ -99,13 +56,9 @@ public class MainActivity extends AppCompatActivity {
     private ServiceConnection mServiceConnection = new ServiceConnection() {
         @Override
         public void onServiceConnected(ComponentName name, IBinder service) {
-            ScatterRoutingService.ScatterBinder binder = (ScatterRoutingService.ScatterBinder) service;
-            mService = binder.getService();
+            binder = ScatterbrainAPI.Stub.asInterface(service);
             mBound = true;
             mServiceToggle.setChecked(true);
-            mStatusTextView.setText("RUNNING");
-            mService.getRadioModule().startAdvertise();
-            mService.getRadioModule().startServer();
             mStatusTextView.setText("DISCOVERING");
             Log.v(TAG, "connected to ScatterRoutingService binder");
         }
@@ -119,14 +72,18 @@ public class MainActivity extends AppCompatActivity {
     };
 
     private void scan() {
-        if (mService != null && mBound) {
-            mService.getRadioModule().startDiscover(BluetoothLEModule.discoveryOptions.OPT_DISCOVER_ONCE);
+        try {
+            binder.startDiscovery();
+        } catch (RemoteException e) {
+            Log.e(TAG, "remoteexception: " + e);
         }
     }
 
     private void stopScan() {
-        if (mService != null && mBound) {
-            mService.getRadioModule().stopDiscover();
+        try {
+            binder.stopDiscovery();
+        } catch (RemoteException e) {
+            Log.e(TAG, "remoteexception " + e);
         }
     }
 
@@ -145,167 +102,14 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void tryP2pConnection(BluetoothLEModule.ConnectionRole role) {
-        final Map<String, String> metadata = new HashMap<>();
-        metadata.putIfAbsent(WifiDirectBootstrapRequest.KEY_NAME, WifiDirectBootstrapRequest.DEFAULT_NAME);
-        byte[] pass = new byte[16];
-        for (byte i=0;i<16;i++) {
-            pass[i] = i;
-        }
-        String p = Base64.encodeToString(pass, Base64.NO_WRAP);
-        Log.e(TAG, "pass: " + p);
-        metadata.putIfAbsent(
-                WifiDirectBootstrapRequest.KEY_PASSPHRASE,
-                BAD_PSK
-                );
-
-        UpgradePacket packet = UpgradePacket.parseFrom(UpgradePacket.newBuilder()
-                .setProvides(AdvertisePacket.Provides.WIFIP2P)
-                .setMetadata(metadata)
-                .setSessionID(1)
-                .build()
-                .writeToStream(20)).blockingGet();
-
-        WifiDirectBootstrapRequest request = WifiDirectBootstrapRequest.create(
-                packet,
-                role
-        );
-
-        if (p2pdisposable != null) {
-            p2pdisposable.dispose();
-        }
-        p2pdisposable = mService.getWifiDirect().bootstrapFromUpgrade(
-                request,
-                Flowable.just(new WifiDirectRadioModule.BlockDataStream(
-                        headerPacket,
-                        Flowable.empty()
-                ))
-        ).subscribe(
-                () -> Log.v(TAG, "successfually transfered blockdata"),
-                err -> {
-                    Log.e(TAG, "failed to transfer blockdata: " + err);
-                }
-        );
-    }
-
-    public void manualServer(RxBleClient client) {
-        mServer = RxBleServer.create(getApplicationContext());
-        ServerConfig config = ServerConfig.newInstance(new Timeout(5, TimeUnit.SECONDS))
-                .addService(BluetoothLERadioModuleImpl.mService);
-
-        mServer.openServer(config)
-                .flatMapCompletable(connectionRaw -> {
-                    CachedLEServerConnection connection = new CachedLEServerConnection(connectionRaw);
-                    RxBleDevice device = client.getBleDevice(connection.getConnection().getDevice().getAddress());
-                    connection.setDefaultReply(
-                            BluetoothLERadioModuleImpl.UUID_LUID,
-                            BluetoothGatt.GATT_SUCCESS
-                    );
-                    connection.serverNotify(
-                            LuidPacket.newBuilder().setLuid(UUID.randomUUID()).enableHashing().build(),
-                            BluetoothLERadioModuleImpl.UUID_LUID
-                    ).subscribe();
-                    return establishConnection(device, new Timeout(10, TimeUnit.SECONDS))
-                            .flatMapSingle(CachedLEConnection::readLuid)
-                            .doOnNext(bytes -> Log.v(TAG, "received luid: " + bytes))
-                            .ignoreElements();
-                })
-                .subscribe(new CompletableObserver() {
-                    @Override
-                    public void onSubscribe(@NonNull Disposable d) {
-                        Log.v(TAG, "gatt server onSubscribe");
-                        manualDisposable = d;
-                    }
-
-                    @Override
-                    public void onComplete() {
-                        Log.v(TAG, "gatt server onComplete");
-                    }
-
-                    @Override
-                    public void onError(@NonNull Throwable e) {
-                        Log.e(TAG, "gatt server onError: " + e);
-                    }
-                });
-
-
-    }
-
-    private Observable<CachedLEConnection> establishConnection(RxBleDevice device, Timeout timeout) {
-
-        Observable<CachedLEConnection> conn = connectionCache.get(device.getMacAddress());
-        if (conn != null) {
-            return conn;
-        }
-        BehaviorSubject<CachedLEConnection> subject = BehaviorSubject.create();
-        connectionCache.put(device.getMacAddress(), subject);
-        return device.establishConnection(false, timeout)
-                .doOnDispose(() -> connectionCache.remove(device.getMacAddress()))
-                .doOnError(err -> connectionCache.remove(device.getMacAddress()))
-                .map(CachedLEConnection::new)
-                .doOnNext(connection -> {
-                    Log.v(TAG, "successfully established connection");
-                    subject.onNext(connection);
-                });
-    }
-
-
-    private void deleteAll(File dir) {
-        if (dir.isDirectory())
-        {
-            String[] children = dir.list();
-            for (int i = 0; i < children.length; i++)
-            {
-                new File(dir, children[i]).delete();
-            }
-        }
-    }
-
-
-    private void wipeDatastore() {
-        mService.getDatastore().clear();
-        File userdir = mService.getDatastore().getUserDir();
-        File cachedir = mService.getDatastore().getCacheDir();
-        deleteAll(userdir);
-        deleteAll(cachedir);
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mBound = false;
 
-        mCreateGroupButton = (Button) findViewById(R.id.creategroupbutton);
-        mConnectGroupButton = (Button) findViewById(R.id.connectgroupbutton);
-
-        mCreateGroupButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                tryP2pConnection(BluetoothLEModule.ConnectionRole.ROLE_UKE);
-            }
-        });
-
-        mDeleteAllButton = (Button) findViewById(R.id.wipedata);
-        mDeleteAllButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Completable.fromAction(() -> wipeDatastore())
-                        .subscribeOn(Schedulers.io())
-                        .subscribe();
-            }
-        });
-
-        mConnectGroupButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                tryP2pConnection(BluetoothLEModule.ConnectionRole.ROLE_SEME);
-            }
-        });
-
-        mStatusTextView = (TextView) findViewById(R.id.status);
-
         mLogsTextView = (TextView) findViewById(R.id.logstextview);
+        mStatusTextView = (TextView) findViewById(R.id.statustext);
         mLogsTextView.setMovementMethod(new ScrollingMovementMethod());
 
         mServiceToggle = (Switch) findViewById(R.id.servicetoggle);
@@ -318,6 +122,8 @@ public class MainActivity extends AppCompatActivity {
                         unbindService(mServiceConnection);
                     }
                 } else {
+                    Intent startIntent = new Intent(getApplicationContext(), ScatterRoutingService.class);
+                    startService(startIntent);
                     Intent bindIntent = new Intent(getApplicationContext(), ScatterRoutingService.class);
                     bindService(bindIntent, mServiceConnection, Context.BIND_AUTO_CREATE);
                 }
@@ -333,67 +139,6 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 mLogsTextView.setText("Scanning...");
                 scan();
-            }
-        });
-
-
-        mManualButton = (Button) findViewById(R.id.manualbutton);
-
-        mManualButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mServer = RxBleServer.create(getApplicationContext());
-                ServerConfig config = ServerConfig.newInstance(new Timeout(5, TimeUnit.SECONDS))
-                        .addService(BluetoothLERadioModuleImpl.mService);
-
-                if (manualDisposable != null) {
-                    manualDisposable.dispose();
-                    manualDisposable = null;
-                    mStatusTextView.setText("Discovering...");
-                    mService.getRadioModule().startServer();
-                    return;
-                }
-
-                mService.getRadioModule().stopDiscover();
-                mService.getRadioModule().stopServer();
-                mStatusTextView.setText("manual gatt");
-                manualServer(RxBleClient.create(getApplicationContext()));
-            }
-        });
-
-        mClientButton = findViewById(R.id.manual_client);
-
-        mClientButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (manualDisposable != null) {
-                    manualDisposable.dispose();
-                    manualDisposable = null;
-                    mStatusTextView.setText("Discovering...");
-                    mService.getRadioModule().startServer();
-                    return;
-                }
-
-                mService.getRadioModule().stopDiscover();
-                mService.getRadioModule().stopServer();
-                mStatusTextView.setText("manual gatt client");
-
-                RxBleClient client = RxBleClient.create(getApplicationContext());
-                manualServer(client);
-
-                client.scanBleDevices(
-                        new ScanSettings.Builder()
-                                .setScanMode(ScanSettings.SCAN_MODE_LOW_POWER)
-                                .setCallbackType(ScanSettings.CALLBACK_TYPE_ALL_MATCHES)
-                                .setShouldCheckLocationServicesState(true)
-                                .build(),
-                        new ScanFilter.Builder()
-                                .setServiceUuid(new ParcelUuid(SERVICE_UUID))
-                                .build())
-                        .concatMap(conn -> {
-                            return establishConnection(conn.getBleDevice(), new Timeout(10, TimeUnit.SECONDS));
-                        }).subscribe();
-
             }
         });
 
